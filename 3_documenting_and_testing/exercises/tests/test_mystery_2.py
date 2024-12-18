36095f423a7b75d01d08940306565281519c6939
Safi222 <safia2000gibril@gmail.com>
2024-12-18 03:18:34 +0300

    practicing unittest

--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -4,3 +4,35 @@ from ..mystery_2 import mystery_2
 
 class TestMystery2(unittest.TestCase):
     """ """
+import unittest
+
+class TestMystery2(unittest.TestCase):
+    """test for a function that returns a length of an string or none"""
+
+    def test1_empty_string(self):
+        """Test with an empty string."""
+        self.assertIsNone(mystery_2(""))
+
+    def test2_non_empty_string(self):
+        """Test with a non-empty string."""
+        self.assertEqual(mystery_2("hello"), 5)
+
+    def test3_empty_list(self):
+        """Test with an empty list."""
+        self.assertIsNone(mystery_2([]))
+
+    def test4_non_empty_list(self):
+        """Test with a non-empty list."""
+        self.assertEqual(mystery_2([1, 2, 3]), 3)
+
+    def test5_tuple(self):
+        """Test with a tuple."""
+        self.assertEqual(mystery_2((6, 8)), 2)
+
+    def test_list_of_lists(self):
+        """Test with a list of lists"""
+        self.assertEqual(mystery_2([[5, 6], [9, 4], [6, 7]]), 3)
+
+    def test_string_with_spaces(self):
+        """Test with a string with spaces and mixed characters"""
+        self.assertEqual(mystery_2("Hi  ,Iam Okay!"), 15)
--- 3_documenting_and_testing/exercises/tests/test_mystery_3.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_3.py
@@ -3,5 +3,9 @@ import unittest
 from ..mystery_3 import mystery_3
 
 
-class TestMystery3(unittest.TestCase):
     """ """
+
+    def test_minimal_input(self):
+        """"""
+        self.assertEqual(mystery_3(0, 0), 0)
+>>>>>>> 33f8c9b08a1d8df52945164a5acdc94a6508e707
